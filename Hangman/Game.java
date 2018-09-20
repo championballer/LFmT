8835cd1e5986c7fa2995cb2c790cf5e0a8288af9
Shiv Kumar <championballer08@gmail.com>
2018-09-20 14:36:15 +0530

    added basic logic to Hangman and applied rename of container folder

---
+++ Hangman/Game.java
@@ -0,0 +1,13 @@
+class Game{
+	private String answer;
+
+	public Game(String answer)
+	{
+		this.answer = answer;
+	}
+
+	public String getAnswer()
+	{
+		return answer;
+	}
+}
--- Hangman/Hangman.java
+++ Hangman/Hangman.java
@@ -2,5 +2,9 @@ public class Hangman{
 	public static void main(String[] args)
 	{
 		System.out.println("Hello World");
+
+		Game game = new Game("treehouse");
+
+		System.out.println(game.getAnswer());
 	}
 }
