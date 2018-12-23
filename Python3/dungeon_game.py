6a9e5eb99287c05799bd8f227eb6b9133f348e97
Shiv Kumar <championballer08@gmail.com>
2018-12-23 20:34:19 +0530

    penultimate dungeon

--- Python3/dungeon_game.py
+++ Python3/dungeon_game.py
@@ -1,5 +1,9 @@
 import random
 
+
+# Random library has a method by the name of sample, that takes as arguments, 
+# an iterable and number of unique values to return from it
+
 #draw grid
 #pick random pos for the player, monster and door
 #draw player in grid
@@ -8,11 +12,63 @@ import random
 #check for win/loss
 #clear screen and redraw grid
 
-def get_locations():
-	monster = None
-	door = None
-	player = None
+CELLS = [(0,0),(0,1),(0,2),(0,3),(0,4),
+		 (1,0),(1,1),(1,2),(1,3),(1,4),
+		 (2,0),(2,1),(2,2),(2,3),(2,4),
+		 (3,0),(3,1),(3,2),(3,3),(3,4),
+		 (4,0),(4,1),(4,2),(4,3),(4,4)]
 
+def get_locations():
+	monster,door,player = random.sample(CELLS,3)
 	return monster,door,player
 
- 
+def print_locations():
+	print(monster,door,player)
+
+def print_grid(grid):
+	for i in range(len(grid)):
+		print(grid[i])
+
+def draw_grid(player):
+	x = player[0]
+	y = player[1]
+	grid = [[0,0,0,0,0],[0,0,0,0,0],[0,0,0,0,0],[0,0,0,0,0],[0,0,0,0,0]]
+	grid[x][y] = 1
+	print_grid(grid)
+
+def possible_moves(player):
+	MOVES = ['LEFT','RIGHT','UP','DOWN']
+	x = player[0]
+	y = player[1]
+
+	if x == 4:
+		MOVES.remove('DOWN')
+	elif x == 0:
+		MOVES.remove('UP')
+	elif y == 0:
+		MOVES.remove('LEFT')
+	elif y == 4:
+		MOVES.remove('RIGHT')
+
+	return MOVES
+
+def update_player(cmd):
+	x = player[0]
+	y = player[1]
+	
+	if cmd == 'RIGHT':
+		
+monster,door,player = get_locations()
+
+while True:
+	draw_grid(player)
+
+	# Print possible moves
+	print(possible_moves(player))
+	# Take input from user regarding move to make, including one for QUIT, if QUIT then break, else continue
+	cmd = input(">")
+	if cmd.upper() == 'QUIT':
+		print("Thanks for playing")
+		break
+	# Based on the input update the player's position, and check if win/lose accordingly decide to break or continue
+	
