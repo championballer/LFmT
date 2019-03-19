dd7bb0450f85a6501a93f8234f7f81ea389efbcd
Shiv Kumar <championballer08@gmail.com>
2019-03-19 06:41:30 +0530

    Completed dynamic rendering and state change in counter app

--- javascript/react/counter-app/src/components/counter.jsx
+++ javascript/react/counter-app/src/components/counter.jsx
@@ -3,22 +3,48 @@ import React, { Component } from 'react';
 class Counter extends Component{
 
 	state = {
-		count : 0
+		count : 0,
+		tags : ['tag1','tag2','tag3']
 	};
+
+
+	styles = {
+		backgroundColor: 'blue',
+		fontSize : 30
+	}
+
+	handleIncrement = () => {
+		this.setState({count:this.state.count+1});
+	}
 
 	render () {
 		return (
 			<div>
-				<h1>{this.countFunc()}</h1>
-				<button>Increment</button>
+				<span className = {this.getBadgeClasses()}>{this.countFunc()}</span>
+				<button onClick = {this.handleIncrement} style = {this.styles} className = "btn btn-secondary btn-sm">Increment</button>
 			</div>
 		);
 	}
 
-	countFunc(){
-		if(this.state.count==0)
+	getBadgeClasses(){
+		let classes = "badge m-2";
+		if(this.state.count===0)
 		{
-			return <h1>Zero</h1>;
+			classes+=" badge-warning";
+		}
+
+		else
+		{
+			classes+=" badge-primary";
+		}
+
+		return classes;
+	}
+
+	countFunc(){
+		if(this.state.count===0)
+		{
+			return "Zero";
 		}
 		else
 			return this.state.count;
