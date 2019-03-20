a7bd00d6b77cf22eb22a0a6db07e3257eba249e8
Shiv Kumar <championballer08@gmail.com>
2019-03-20 22:05:43 +0530

    Added ajax xml

---
+++ javascript/ajax/xml.js
@@ -0,0 +1,13 @@
+var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
+var XHR = new XMLHttpRequest();
+
+XHR.onreadystatechange = () => {
+	if(XHR.readyState===4)
+	{
+		console.log(XHR.responseText);
+	}
+}
+
+XHR.open("GET","https://api.github.com/zen");
+XHR.send();
+
