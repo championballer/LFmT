542a45f0e566d65f3b70914ce843c488ab47e5e3
Shiv Kumar <championballer08@gmail.com>
2019-08-07 15:01:47 +0530

    Modifications2

--- javascript/async/callbackan.js
+++ javascript/async/callbackan.js
@@ -1,13 +1,22 @@
-function greet(name,formatter)
+function greet(val,name,formatter)
 {
-	console.log("Hello "+formatter(name));
+	console.log("Hello "+formatter(name,val));
 }
 
-greet("Shiv",(name)=>{
+greet(0,"Shiv",(name,val)=>{
+	console.log(val);
 	return name.toUpperCase();
 });
 
-greet("Agrima",(name)=>{
+function sample(name,val){
+	console.log(val);
+	return name.toUpperCase();
+}
+
+greet(1,"Agrima",(name,val)=>{
+	console.log(val);
 	return name;
 });
+
+greet(2,"Hriday",sample);
 
