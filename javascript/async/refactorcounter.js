4bc802127f329b79013f187294ba355b41ece63f
Shiv Kumar <championballer08@gmail.com>
2019-08-09 12:10:01 +0530

    Added promise chaining code

--- javascript/async/refactorcounter.js
+++ javascript/async/refactorcounter.js
@@ -3,6 +3,7 @@ let counter = 0;
 function increment(){
 	counter++;
 	console.log(counter);
+	return counter+1;
 }
 
 function pchain(callback,time){
@@ -16,8 +17,12 @@ function pchain(callback,time){
 	return p;
 }
 
-pchain(increment,1000).then(function(){
+pchain(increment,1000).then(function(current){
+	console.log(current);
 	return pchain(increment,2000);
-}).then(function(){
+}).then(function(current){
+	console.log(current);
 	return pchain(increment,3000);
+}).then(function(current){
+	console.log(current);
 });
